a6f9f05f0dc8cb0d2c9fcb2442ed722aff00eb7e
gauravtheP <gamesleo89@gmail.com>
2017-08-22 19:28:09 +0530

    Merge sort code modified.

--- src/MiscellaneousJavaPrograms/MergeSort/MergeSort.java
+++ src/MiscellaneousJavaPrograms/MergeSort/MergeSort.java
@@ -4,63 +4,67 @@
 
 package MiscellaneousJavaPrograms.MergeSort;
 
+import java.util.Scanner;
+
 public class MergeSort {
-    private int[] array;
-    private int[] tempMergeArray;
-    private int length;
 
-    public static void main(String args[]) {
-        int[] inputArray = {5,4,3,2,1};
-        MergeSort ms = new MergeSort();
-        ms.sort(inputArray);
-        System.out.println("Sorted array elements are:");
-        for (int i = 0; i < ms.length; i++) {
-            System.out.print(inputArray[i] + " ");
+    void Merging(int array[], int first, int middle, int last){
+        int n1 = middle - first + 1;
+        int n2 = last - middle;
+        int left[] = new int[n1+1];
+        int right[] = new int[n2+1];
+        int i, j, k;
+        for(i = 0; i<n1; i++){
+            left[i] = array[first + i];
         }
-    }
-
-    public void sort(int inputArray[]) {
-        this.array = inputArray;
-        this.length = inputArray.length;
-        this.tempMergeArray = new int[length+2];
-        doMergeSort(0, length - 1);
-    }
-
-    private void doMergeSort(int lowerIndex, int higherIndex) {
-        if (lowerIndex < higherIndex) {
-            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
-            // Below step sorts the left side of the array
-            doMergeSort(lowerIndex, middle);
-            // Below step sorts the right side of the array
-            doMergeSort(middle + 1, higherIndex);
-            // Below step are merging the array
-            mergeParts(lowerIndex, middle, higherIndex);
+        for(j = 0; j<n2; j++){
+            right[j] = array[middle + j + 1];
         }
-    }
-
-    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
-        for (int i = lowerIndex; i <= higherIndex; i++) {
-            tempMergeArray[i] = array[i];
-            System.out.print(tempMergeArray[i]+" ");
-        }
-        System.out.println();
-        int i = lowerIndex;
-        int j = middle + 1;
-        int k = lowerIndex;
-        while (i <= middle && j <= higherIndex)   //This is while loop.
-        {
-            if (tempMergeArray[i] <= tempMergeArray[j]) {
-                array[k] = tempMergeArray[i];
-                i++;
-            } else {
-                array[k] = tempMergeArray[j];
-                j++;
+        left[n1] = 2147483646;
+        right[n2] = 2147483646;
+        i = 0;
+        j = 0;
+        for(k = first; k<last+1; k++){
+            if(left[i] <= right[j]){
+                array[k] = left[i];
+                i = i+1;
             }
-            k++;
+            else{
+                array[k] = right[j];
+                j = j+1;
+            }
         }
-        for(i=lowerIndex;i<=middle; i++) {
-            array[k] = tempMergeArray[i];
-            k++;
+    }
+
+    void MergeSorting(int array[], int first, int last){
+        int middle;
+        if(first < last){
+            middle = (first + last)/2;
+            MergeSorting(array, first, middle);
+            MergeSorting(array, middle+1, last);
+            Merging(array, first, middle, last);
+        }
+    }
+
+    public static void main(String[] args){
+        MergeSort sort = new MergeSort();
+        Scanner sc = new Scanner(System.in);
+        int i, size;
+
+        System.out.print("Enter the size of array\n");
+        size = sc.nextInt();
+        int array[] = new int[size];
+        System.out.print("Enter array Elements\n");
+        for(i=0; i<size; i++){
+            array[i]=sc.nextInt();
+        }
+
+        int first = 0, last = size-1;
+        System.out.print("Sorted Array: ");
+        sort.MergeSorting(array, first, last);
+
+        for(i = 0; i<size; i++){
+            System.out.print(array[i] + " ");
         }
     }
 }
