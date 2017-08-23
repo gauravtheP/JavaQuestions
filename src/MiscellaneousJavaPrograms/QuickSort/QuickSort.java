63e36347a2dae3c8719177964514ba1803d78ce2
gauravtheP <gamesleo89@gmail.com>
2017-08-23 07:11:22 +0530

    Quick sort code modified.

--- src/MiscellaneousJavaPrograms/QuickSort/QuickSort.java
+++ src/MiscellaneousJavaPrograms/QuickSort/QuickSort.java
@@ -3,21 +3,13 @@
  */
 package MiscellaneousJavaPrograms.QuickSort;
 
+import java.util.Scanner;
+
 public class QuickSort {
     private int[] array;
     private int length;
 
-    public static void main(String args[]) {
-        int[] inputArray = {6,5,4,3,2,1};
-        QuickSort qs = new QuickSort();
-        qs.sort(inputArray);
-        System.out.println("Sorted array elements are:");
-        for (int i = 0; i < qs.length; i++) {
-            System.out.print(inputArray[i] + " ");
-        }
-    }
-
-    public void sort(int inputArray[]) {
+    private void sort(int inputArray[]) {
         this.array = inputArray;
         this.length = inputArray.length;
         doQuickSort(0, length - 1);
@@ -27,19 +19,14 @@ public class QuickSort {
         if (lowerIndex < higherIndex) {
             int q;
             q = Partition(lowerIndex, higherIndex);
-            System.out.println("Returned Value of q is: "+q);
-            System.out.println();
             doQuickSort(lowerIndex,q-1);
             doQuickSort(q + 1, higherIndex);
         }
     }
+
     private int Partition(int lowerIndex, int higherIndex){
-        for (int i = lowerIndex; i <= higherIndex; i++) {
-            System.out.print(array[i]+" ");
-        }
-            System.out.println();
+
         int i = lowerIndex - 1;
-        System.out.println("Pivot is: "+array[higherIndex]);
         for(int j=lowerIndex; j<=higherIndex-1; j++) {
             if (array[j] <= array[higherIndex]) {
                 i = i + 1;
@@ -52,6 +39,25 @@ public class QuickSort {
         array[higherIndex] = array[i + 1];
         array[i + 1] = temp2;
         return i + 1;
+    }
+
+    public static void main(String args[]) {
+        QuickSort qs = new QuickSort();
+        Scanner sc = new Scanner(System.in);
+        int i, size;
+        System.out.print("Enter the size of array\n");
+        size = sc.nextInt();
+        int array[] = new int[size];
+        System.out.print("Enter array Elements\n");
+        for(i=0; i<size; i++){
+            array[i]=sc.nextInt();
+        }
+        qs.sort(array);
+
+        System.out.print("Sorted array elements are: ");
+        for (i = 0; i < qs.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
 //here above "pivot" element is the last element of array every time means
